0b866e20d9c67d855b1f2b073ebe8fc104a4c981
PierreMonvoisin <monvoisin.pierre@gmail.com>
2019-11-12 14:38:48 +0100

    Exercice terminé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
 $(function() {
-
+  $('#fName, #lName').keyup(function nameCheck() {
+    let input = ($(this).val()).trim()
+    let pattern = /^[a-zA-Z.'-]*$/;
+    if (pattern.test(input) && input != '') {
+      $(this).css('border', '2px solid green')
+    } else {
+      $(this).css('border', '3px solid red')
+    }
+  });
+  $('#mail').keyup(function mailCheck() {
+    let input = ($(this).val()).trim();
+    let pattern = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (pattern.test(input) && input != ''){
+      $(this).css('border', '2px solid green')
+    } else {
+      $(this).css('border', '3px solid red')
+    }
+  });
+  $('#phoneNumber').keyup(function phoneCheck() {
+    let input = ($(this).val()).trim();
+    let pattern = /^[+]*[(]{0,1}[0-9]{0,4}[)]{0,1}[0-9]*$/;
+    if (pattern.test(input) && input != ''){
+      $(this).css('border', '2px solid green')
+    } else {
+      $(this).css('border', '3px solid red')
+    }
+  });
 });
